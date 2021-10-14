65ffca28fe62639ea7b0abfb4488165c4eea3e2c
Santiago Díaz Guevara <santidiazg@hotmail.es>
2021-10-13 17:12:26 -0700

    Rename and reposition games files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const home = require('./controllers/home')
-const game = require('./controllers/game')
+const games = require('./controllers/games')
 const api  = require('./controllers/api/api')
 
 const app = express()
@@ -8,7 +8,7 @@ const port = 3000
 
 app.use('/', home)
 app.use('/api', api)
-app.use('/games', game)
+app.use('/games', games)
 app.use(express.static('src/views/public'))
 
 app.listen(port, () => {
