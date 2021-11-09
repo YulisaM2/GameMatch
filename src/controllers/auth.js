fcc622e58c83790ce97a39728abade41ae97d6f1
ym2 <a01570056@itesm.mx>
2021-11-08 20:01:37 -0600

    Logout added

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,14 +2,10 @@ var express = require('express')
 const path = require('path')
 const User = require('../models/user')
 const passport = require('passport')
+const { isLoggedIn } = require('../middleware')
+
 
 var router = express.Router()
-
-// router.get('/fakeUser', async (req, res) => {
-//     const user = new User({email: 'prueba2@gmail.com', username: 'prueba2', name:'Prueba2'})
-//     const newUser = await User.register(user, 'test')
-//     res.send(newUser)
-// })
 
 router.get('/register', (req, res) => {
     res.render('./auth/Register')
@@ -43,6 +39,12 @@ router.post('/login', passport.authenticate('local', { failureFlash: false, fail
     console.log("Logged in")
     res.redirect('/')
 })
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
 
 router.get('/forgot', (req, res) => {
     res.render('./auth/Forgot')
