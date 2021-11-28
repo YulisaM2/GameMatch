9dcd2835109f958327c689f1f8dbf1c2bb0424a4
Santiago Díaz Guevara <santidiazg@hotmail.es>
2021-11-27 21:06:39 -0700

    Remove console logs from service worker

--- src/views/public/sw.js
+++ src/views/public/sw.js
@@ -1,11 +1,3 @@
-self.addEventListener('install', event => {
-    console.log('WORKER: install event in progress.');
-});
+self.addEventListener('install', event => {});
 
-self.addEventListener('fetch', event => {
-    console.log('WORKER: Fetching', event.request);
-
-    // event.respondWith(
-    //     fetch(event.request)
-    // );
-});
+self.addEventListener('fetch', event => {});
